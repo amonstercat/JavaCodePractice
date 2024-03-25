3ca583c9b478a04461be95c065bfab440265304c
amonstercat <amonstercattt@gmail.com>
2024-03-25 23:53:23 +0800

    测试单机限流与分布式集群限流

--- src/main/java/com/example/codepractice/GuavaLimiter.java
+++ src/main/java/com/example/codepractice/GuavaLimiter.java
@@ -1,4 +1,22 @@
 package com.example.codepractice;
 
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class GuavaLimiter {
+
+    public static void main(String[] args) {
+        String start = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        RateLimiter limiter = RateLimiter.create(5); // 这里的1表示每秒允许处理的量为5个
+        for (int i = 1; i <= 10; i++) {
+            limiter.acquire();// 请求RateLimiter, 超过permits会被阻塞
+            System.out.println("call execute.." + i);
+        }
+        String end = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        System.out.println("start time:" + start);
+        System.out.println("end time:" + end);
+    }
+
 }
